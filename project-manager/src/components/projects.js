47fb58678b3a94bff6642079c4414447f13e2758
Brendon Richards <brendonrichards93@gmail.com>
2018-09-25 06:58:37 +0000

    Create projects data

---
+++ project-manager/src/components/projects.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div className="Projects">
+        <h1>My Projects</h1>
+      </div>
+    );
+  }
+}
+
+export default Projects;
